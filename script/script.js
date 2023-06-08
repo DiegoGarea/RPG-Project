70f7cbcdad93b9bf75467a87049d88cc572b4576
Diego G <d.g.g.87@hotmail.com>
2023-06-08 13:54:12 -0600

    4th update

--- script/script.js
+++ script/script.js
@@ -88,9 +88,9 @@ const locations = [
     'button text': [
       'Go to town square',
       'Go to town square',
-      'Go to town square',
+      'Go to town square??',
     ],
-    'button functions': [goTown, goTown, goTown],
+    'button functions': [goTown, goTown, easterEgg],
     text: 'The monster screams "Arg!" as it dies. You gain experience point and find gold.',
   },
   {
@@ -104,6 +104,12 @@ const locations = [
     'button text': ['REPLAY?', 'REPLAY?', 'REPLAY?'],
     'button functions': [restart, restart, restart],
     text: 'You defeat the dragon! YOU WIN THE GAME!',
+  },
+  {
+    name: 'easter egg',
+    'button text': ['2', '8', 'Go to town square'],
+    'button functions': [pickTwo, pickEight, goTown],
+    text: 'You find a secret game. Pick a number above. ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!',
   },
 ];
 
@@ -271,4 +277,43 @@ function restart() {
   healthText.innerText = health;
   xpText.innerText = xp;
   goTown();
+}
+
+function easterEgg() {
+  update(locations[7]);
+}
+
+function pickTwo() {
+  pick(2);
+}
+
+function pickEight() {
+  pick(8);
+}
+
+function pick(guess) {
+  let numbers = [];
+
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+
+  text.innerText = 'You picked ' + guess + '. Here are the random numbers:\n';
+
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + '\n';
+  }
+
+  if (numbers.indexOf(guess) !== -1) {
+    text.innerText += 'Right! You win 20 gold.';
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += 'Wrong! You lose 10 health';
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
 }
